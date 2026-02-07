cd67961e46f27eb0528b389793ee478c918b4c30
chrimaho <44449504+chrimaho@users.noreply.github.com>
2026-02-08 08:24:33 +1100

    Update Formatting Script for Querying Data - Modify the script to extract sections from `querying-data.md` for various data types. - Adjust the conversion commands to use `querying-data.md` and corresponding notebook files.

--- docs/querying-data/src/cli/formatting.sh
+++ docs/querying-data/src/cli/formatting.sh
@@ -1,10 +1,10 @@
 uv run lint
-uv run extract-sections-from-markdown-file index.md pandas
-uv run extract-sections-from-markdown-file index.md sql
-uv run extract-sections-from-markdown-file index.md pyspark
-uv run extract-sections-from-markdown-file index.md polars
-uv run reformat-and-convert-md-to-ipynb index.md h3
-uv run reformat-and-convert-md-to-ipynb index-pandas.md h3
-uv run reformat-and-convert-md-to-ipynb index-sql.md h3
-uv run reformat-and-convert-md-to-ipynb index-pyspark.md h3
-uv run reformat-and-convert-md-to-ipynb index-polars.md h3
+uv run extract-sections-from-markdown-file querying-data.md pandas notebooks
+uv run extract-sections-from-markdown-file querying-data.md sql notebooks
+uv run extract-sections-from-markdown-file querying-data.md pyspark notebooks
+uv run extract-sections-from-markdown-file querying-data.md polars notebooks
+uv run reformat-and-convert-md-to-ipynb querying-data.md h3 notebooks
+uv run reformat-and-convert-md-to-ipynb notebooks/querying-data-pandas.md h3
+uv run reformat-and-convert-md-to-ipynb notebooks/querying-data-sql.md h3
+uv run reformat-and-convert-md-to-ipynb notebooks/querying-data-pyspark.md h3
+uv run reformat-and-convert-md-to-ipynb notebooks/querying-data-polars.md h3
